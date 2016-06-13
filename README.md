14520e2a19944c532a69f800a2138ac0f1d377f9
efernie <ericf2023@gmail.com>
2016-06-12 22:52:46 -0400

    stuff

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Backflip-Android-Webview
 
 - This project was generated using Android Studio (Preview) 0.4.6
+- This webview is using code targeted at 4.0 (ICE_CREAM_SANDWICH) and above
--- backflip/src/main/java/com/backflip/backflip_android_webview/backflip/MainActivity.java
+++ backflip/src/main/java/com/backflip/backflip_android_webview/backflip/MainActivity.java
@@ -15,77 +15,77 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    private WebView web;
+  private WebView web;
 
-    // Logging
-    private Boolean VERBOSE = true;
-    private String TAG = "Backflip";
+  // Logging
+  private Boolean VERBOSE = true;
+  private String TAG = "Backflip";
 
-    private final String localhostProtocol = "http://";
-    private final String localhostUrl = "192.168.1.137";
-    private final String localhostPort = ":3000";
-    private final String fullLocalhostUrl = localhostProtocol + localhostUrl + localhostPort;
+  private final String localhostProtocol = "http://";
+  private final String localhostUrl = "192.168.1.137";
+  private final String localhostPort = ":3000";
+  private final String fullLocalhostUrl = localhostProtocol + localhostUrl + localhostPort;
 
-    public static Context context;
+  public static Context context;
 
-    private final WebViewClient webClient = new WebViewClient() {
+  private final WebViewClient webClient = new WebViewClient() {
 
-      @Override
-      public boolean shouldOverrideUrlLoading( WebView view, String url ) {
+    @Override
+    public boolean shouldOverrideUrlLoading( WebView view, String url ) {
 
-        if ( Uri.parse(url).getHost().equals(localhostUrl) ) {
-          return false;
-        }
+      if ( Uri.parse(url).getHost().equals(localhostUrl) ) {
+        return false;
+      }
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        startActivity(intent);
+      Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+      startActivity(intent);
+      return true;
+    }
+  };
+
+
+  @SuppressLint("SetJavaScriptEnabled")
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    MainActivity.context = getApplicationContext();
+
+    setContentView(R.layout.activity_main);
+
+    web = (WebView)findViewById(R.id.web);
+    web.getSettings().setJavaScriptEnabled(true);
+    web.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+    web.getSettings().setDomStorageEnabled(true);
+    web.getSettings().setAllowFileAccess(true);
+
+    web.addJavascriptInterface(new BackflipJavascriptInterface(this), "Android");
+    web.setWebViewClient(webClient);
+
+    web.loadUrl(fullLocalhostUrl);
+
+    // To catch the console logs and display them in the logcat
+    web.setWebChromeClient(new WebChromeClient() {
+      public boolean onConsoleMessage(ConsoleMessage cm) {
+        if (VERBOSE) Log.d(TAG, cm.message() + " -- From line "
+          + cm.lineNumber() + " of "
+          + cm.sourceId() );
         return true;
       }
-    };
+    });
+  }
 
+  public class BackflipJavascriptInterface {
+    final Context mContext;
 
-    @SuppressLint("SetJavaScriptEnabled")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        MainActivity.context = getApplicationContext();
-
-        setContentView(R.layout.activity_main);
-
-        web = (WebView)findViewById(R.id.web);
-        web.getSettings().setJavaScriptEnabled(true);
-        web.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        web.getSettings().setDomStorageEnabled(true);
-        web.getSettings().setAllowFileAccess(true);
-
-        web.addJavascriptInterface(new BackflipJavascriptInterface(this), "Android");
-        web.setWebViewClient(webClient);
-
-       web.loadUrl(fullLocalhostUrl);
-
-      // To catch the console logs and display them in the logcat
-      web.setWebChromeClient(new WebChromeClient() {
-        public boolean onConsoleMessage(ConsoleMessage cm) {
-          if (VERBOSE) Log.d(TAG, cm.message() + " -- From line "
-            + cm.lineNumber() + " of "
-            + cm.sourceId() );
-          return true;
-        }
-      });
+    BackflipJavascriptInterface(Context c) {
+      mContext = c;
     }
 
-    public class BackflipJavascriptInterface {
-      final Context mContext;
+    @JavascriptInterface
+    // TODO: add code to take picture and return it to the webapp
+    public void takePicture () {
 
-      BackflipJavascriptInterface(Context c) {
-        mContext = c;
-      }
-
-      @JavascriptInterface
-      // TODO: add code to take picture and return it to the webapp
-      public void takePicture () {
-
-      }
     }
+  }
 }
